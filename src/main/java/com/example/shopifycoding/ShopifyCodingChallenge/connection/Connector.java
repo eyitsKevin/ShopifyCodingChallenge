bbe2f62c2d49d6bbc856b80a649b8a1ef4bd8220
eyitsKevin <kevinateur@hotmail.com>
2019-01-21 02:07:44 -0500

    Add connection to db

--- src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/connection/Connector.java
+++ src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/connection/Connector.java
@@ -1,4 +1,41 @@
 package com.example.shopifycoding.ShopifyCodingChallenge.connection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Connector {
+
+    private ResultSet resultSet;
+    private Connection connection;
+    private Statement statement;
+
+    public Connector(ResultSet resultSet, Connection connection, Statement statement){
+        this.resultSet = resultSet;
+        this.connection = connection;
+        this.statement = statement;
+    }
+
+    // Getters
+    public Connection getConnection(){
+        return this.connection;
+    }
+    public ResultSet getResultSet(){
+        return this.resultSet;
+    }
+    public Statement getStatement() { return this.statement; }
+
+
+    // closes all connection
+    // mainly used in other classes after having done their queries
+    public void close(){
+        try{
+            this.statement.close();
+            this.connection.close();
+            this.resultSet.close();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/connection/DbConnection.java
+++ src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/connection/DbConnection.java
@@ -1,4 +1,39 @@
 package com.example.shopifycoding.ShopifyCodingChallenge.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DbConnection {
+
+    // returns a Connector Object containing the connection, the resultset and the statement
+    public static Connector get(String query){
+        Connector conn = null;
+        try {
+            Connection con = connect();
+            // Database Connection
+            Statement stmt = con.createStatement();
+            // SQL Query to search
+            ResultSet rs = stmt.executeQuery(query);
+
+            conn = new Connector(rs, con, stmt);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public static Connection connect(){
+        Connection connection = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:hsqldb:file:K:/My Workspace/ShopifyCodingChallenge/src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/connection\\marketplacedb/", "user", "useruser");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
