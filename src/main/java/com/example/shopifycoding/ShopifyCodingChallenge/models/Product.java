3b8b5ded2619edcd12f504026b89cd9137937d54
eyitsKevin <kevinateur@hotmail.com>
2019-01-21 02:07:52 -0500

    Add product model

--- src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/models/Product.java
+++ src/main/java/com/example/shopifycoding/ShopifyCodingChallenge/models/Product.java
@@ -7,40 +7,48 @@ import java.util.Objects;
  */
 public class Product {
 
+    private int id;
     private String title;
     private double price;
     private int inventory_count;
 
-    public Product() {
-        this.title = "missing title";
-        this.price = 0.0;
-        this.inventory_count = 0;
-    }
-
-    public Product(String title, double price, int inventory_count) {
+    public Product(int id, String title, double price, int inventory_count) {
+        this.id = id;
         this.title = title;
         this.price = price;
         this.inventory_count = inventory_count;
+    }
+
+    /**
+     * Accessors and mutators
+     */
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public int getInventory_count() {
         return inventory_count;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public void setInventory_count(int inventory_count) {
@@ -52,13 +60,33 @@ public class Product {
         if (this == o) return true;
         if (!(o instanceof Product)) return false;
         Product product = (Product) o;
-        return Double.compare(product.getPrice(), getPrice()) == 0 &&
+        return getId() == product.getId() &&
+                Double.compare(product.getPrice(), getPrice()) == 0 &&
                 getInventory_count() == product.getInventory_count() &&
                 Objects.equals(getTitle(), product.getTitle());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTitle(), getPrice(), getInventory_count());
+        return Objects.hash(getId(), getTitle(), getPrice(), getInventory_count());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", inventory_count=" + inventory_count +
+                '}';
+    }
+
+    public Boolean checkout() {
+        if (inventory_count > 0) {
+            this.inventory_count--;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
